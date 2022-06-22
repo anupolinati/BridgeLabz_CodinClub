30f2f8c42d8b72235cba5b19edba9313102b6f23
anupolinati <107063944+anupolinati@users.noreply.github.com>
2022-06-22 18:23:46 +0530

    Add files via upload

--- Arrayduplicates.sh
+++ Arrayduplicates.sh
@@ -1,15 +1,15 @@
-int =("10 20" "20 10" "10 20" "5 20")
+int ="$("10 20" "20 10" "10 20" "5 20")"
 echo "Array is ${!int[@]}"
 echo "*****create*****"
-int[4]=("10 3")
-int[5]=("20 4")
-int[6]=("5 1")
+int[4]="$("10 3")"
+int[5]="$("20 4")"
+int[6]="$("5 1")"
 echo "After Added Array is ${int[*]}"
 echo "*****update *****"
-int[4]=("10 3")
-int[5]=("20 4")
-int[6]=("5 1")
-echo "After update , Array i ${int[*]}"
+int[4]="$("10 3")"
+int[5]="$("20 4")"
+int[6]="$("5 1")"
+echo "After update , Array is ${int[*]}"
 
 
 
